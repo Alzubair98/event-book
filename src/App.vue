95c41546c9bc7560a8a0982f76e7c7610ef00a62
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-12-04 12:02:08 +0300

    add props

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,14 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
-    <section class="grid grid-cols-2 gap-2">
-      <EventCard v-for="i in 8" :key="i" />
+    <section class="grid grid-cols-2 gap-8">
+      <EventCard
+        v-for="i in 8"
+        :key="i"
+        title="event2"
+        date="today"
+        description="important"
+      />
     </section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
   </main>
--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="border border-gray-800 bg-white rounded-md">
-    <h3 class="p-4 font-medium text-xl border-b border-gray-200">what</h3>
-    <p class="p-4 border-b border-gray-200">when</p>
-    <p class="p-4">Description</p>
+    <h3 class="p-4 font-medium text-xl border-b border-gray-200">
+      {{ title }}
+    </h3>
+    <p class="p-4 border-b border-gray-200">{{ date }}</p>
+    <p class="p-4">{{ description }}</p>
   </div>
 </template>
 
 <script lang="ts">
-export default {
+import { defineComponent } from "vue";
+
+export default defineComponent({
   name: "EventCard",
-};
+  props: {
+    title: String,
+    date: String,
+    description: String,
+  },
+});
 </script>
