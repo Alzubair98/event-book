71c1f8396ee605ca05d32fd4c34279ea80172cc0
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-12-04 11:30:01 +0300

    show comps in the app page

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,19 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
+    <section class="grid grid-cols-2 gap-2">
+      <EventCard v-for="i in 8" :key="i" />
+    </section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
   </main>
 </template>
 
 <script lang="ts">
 import EventCard from "./components/EventCard.vue";
+
+export default {
+  components: {
+    EventCard,
+  },
+};
 </script>
--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,4 +1,10 @@
-<template><div>Component</div></template>
+<template>
+  <div class="border border-gray-800 bg-white rounded-md">
+    <h3 class="p-4 font-medium text-xl border-b border-gray-200">what</h3>
+    <p class="p-4 border-b border-gray-200">when</p>
+    <p class="p-4">Description</p>
+  </div>
+</template>
 
 <script lang="ts">
 export default {
