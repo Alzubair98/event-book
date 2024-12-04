f488e976532f15c67a33c879803cfe700bfe34a3
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-12-04 10:12:19 +0300

    add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  // Make changes below
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
