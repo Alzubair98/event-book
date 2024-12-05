c6e06110e3331a5469658e873baf63bf25987cad
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-12-05 12:23:08 +0300

    use eventcards

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,26 +1,28 @@
 <template>
-  <rounded-card>
-    <h3 class="p-4 font-medium text-xl border-b border-gray-200">
-      {{ title }}
-    </h3>
-    <p class="p-4 border-b border-gray-200">{{ date }}</p>
-    <p class="p-4">{{ description }}</p>
-    <section class="flex justify-end p-4">
-      <newbutton label="Rigster" @click="$emit('register')">Rigster</newbutton>
-    </section>
-  </rounded-card>
+  <sectionCard>
+    <template #header>{{ title }}</template>
+    {{ date }}
+    <template #footer>
+      <p>{{ description }}</p>
+      <div class="flex justify-end p-4">
+        <newbutton label="Rigster" @click="$emit('register')"
+          >Rigster</newbutton
+        >
+      </div></template
+    >
+  </sectionCard>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import newbutton from "./NewButton.vue";
-import RoundedCard from "./RoundedCard.vue";
+import sectionCard from "./sectionCard.vue";
 
 export default defineComponent({
   name: "EventCard",
   components: {
     newbutton,
-    RoundedCard,
+    sectionCard,
   },
   props: {
     title: String,
--- src/components/sectionCard.vue
+++ src/components/sectionCard.vue
@@ -1,8 +1,15 @@
 <template>
   <RoundedCard>
-    <slot name="header"></slot>
-    <slot></slot>
-    <slot name="footer"></slot>
+    <h3 class="p-4 font-medium text-xl border-b border-gray-200">
+      <slot name="header"></slot>
+    </h3>
+    <div class="p-4 border-b border-gray-200">
+      <slot></slot>
+    </div>
+
+    <div class="p-4">
+      <slot name="footer"></slot>
+    </div>
   </RoundedCard>
 </template>
 
