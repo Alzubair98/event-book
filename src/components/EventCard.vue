c69ce1fd43c0562388961ad7b889d913d2a5e97d
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-12-05 10:02:27 +0300

    add rounded card

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,30 +1,26 @@
 <template>
-  <div class="border border-gray-800 bg-white rounded-md">
+  <rounded-card>
     <h3 class="p-4 font-medium text-xl border-b border-gray-200">
       {{ title }}
     </h3>
     <p class="p-4 border-b border-gray-200">{{ date }}</p>
     <p class="p-4">{{ description }}</p>
     <section class="flex justify-end p-4">
-      <!-- <button
-        class="text-sm font-medium border border-gray-200 rounded-full px-3 py-1 hover:bg-gray-100"
-        @click="$emit('register')"
-      >
-        Register
-      </button> -->
       <newbutton label="Rigster" @click="$emit('register')">Rigster</newbutton>
     </section>
-  </div>
+  </rounded-card>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import newbutton from "./NewButton.vue";
+import RoundedCard from "./RoundedCard.vue";
 
 export default defineComponent({
   name: "EventCard",
   components: {
     newbutton,
+    RoundedCard,
   },
   props: {
     title: String,
