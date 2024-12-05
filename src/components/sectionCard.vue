714f9fe3918601f336ca2d8be14bd6db3524c5b2
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-12-05 11:06:47 +0300

    add section comp

---
+++ src/components/sectionCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <RoundedCard>
+    <slot name="header"></slot>
+    <slot></slot>
+    <slot name="footer"></slot>
+  </RoundedCard>
+</template>
+
+<script lang="ts">
+import RoundedCard from "./RoundedCard.vue";
+export default {
+  name: "sectionCard",
+  components: {
+    RoundedCard,
+  },
+};
+</script>
