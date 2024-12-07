207299843f23c0337cce6ed2f3614608c8bb36ce
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-12-07 14:01:16 +0300

    add loading logic

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
-    <section class="grid grid-cols-2 gap-8">
+    <section class="grid grid-cols-2 gap-8" v-if="!eventsLoading">
       <EventCard
         v-for="event in events"
         :key="event.id"
@@ -12,6 +12,7 @@
         @register="console.log('Registered!')"
       />
     </section>
+    <section v-else>Loading ...</section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
     <section class="flex flex-col gap-6">
       <BookingCard v-for="i in 3" :key="i">This is the first card</BookingCard>
@@ -36,6 +37,7 @@ export default {
   data() {
     return {
       events: ref<Event[]>([]),
+      eventsLoading: ref(false),
     };
   },
 
@@ -47,12 +49,14 @@ export default {
   methods: {
     async fetchEvents() {
       try {
+        this.eventsLoading = true;
         const response = await fetch("http://localhost:3001/events");
         this.events = await response.json();
       } catch (error) {
         console.log("Failed to fetch events;", error);
+      } finally {
+        this.eventsLoading = false;
       }
-      console.log(this.events);
     },
   },
   mounted() {
