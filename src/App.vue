10f13fde24454484c41492d8f458319ae6af8dd3
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-12-07 14:13:40 +0300

    add the hold cards

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,19 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
-    <section class="grid grid-cols-2 gap-8" v-if="!eventsLoading">
-      <EventCard
-        v-for="event in events"
-        :key="event.id"
-        :title="event.title"
-        :date="event.date"
-        :description="event.description"
-        @register="console.log('Registered!')"
-      />
+    <section class="grid grid-cols-2 gap-8">
+      <template v-if="!eventsLoading">
+        <EventCard
+          v-for="event in events"
+          :key="event.id"
+          :title="event.title"
+          :date="event.date"
+          :description="event.description"
+          @register="console.log('Registered!')"
+      /></template>
+      <template v-else><LoadingEventCard v-for="i in 4" :key="i" /></template>
     </section>
-    <section v-else>Loading ...</section>
+
     <h2 class="text-2xl font-medium">Your Bookings</h2>
     <section class="flex flex-col gap-6">
       <BookingCard v-for="i in 3" :key="i">This is the first card</BookingCard>
@@ -23,6 +25,7 @@
 <script lang="ts">
 import EventCard from "./components/EventCard.vue";
 import BookingCard from "./components/BookingItem.vue";
+import LoadingEventCard from "./components/LoadingEventCard.vue";
 import { ref } from "vue";
 
 interface Event {
@@ -44,6 +47,7 @@ export default {
   components: {
     EventCard,
     BookingCard,
+    LoadingEventCard,
   },
 
   methods: {
